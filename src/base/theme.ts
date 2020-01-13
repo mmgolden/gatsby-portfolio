f14082128011a87603789e808bde7b77a976d74d
Melinda Golden <info@melindagolden.com>
2020-01-12 21:55:16 -0500

    add: Fonts

--- src/base/theme.ts
+++ src/base/theme.ts
@@ -1,7 +1,15 @@
+import 'typeface-open-sans';
+import 'typeface-playfair-display';
+
 export const theme = Object.freeze({
   colors: {
     white: '#ffffff',
     primary: '#3AC0C8',
+    heading: '#333333',
+  },
+  fonts: {
+    heading: ['serif', 'Playfair Display'],
+    primary: ['sans-serif', 'Open Sans'],
   },
 });
 
