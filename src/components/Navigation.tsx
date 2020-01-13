9a8433794d6edaa7fcffbce0808abf25c50b1b57
Melinda Golden <info@melindagolden.com>
2020-01-12 21:56:44 -0500

    add: Navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,53 @@
+/** @jsx jsx */
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
+import { jsx } from '@emotion/core';
+import { Theme } from '../base/theme';
+import Logo from '../assets/logo-dark.svg';
+import { Burger } from './Burger';
+import { Social } from './Social';
+import { Menu } from './Menu';
+
+export const Navigation: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav css={styles.navbar}>
+      <Link to="/" aria-label="Logo">
+        <Logo css={styles.logo} />
+      </Link>
+      <Burger isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <Social />
+    </nav>
+  );
+};
+
+const styles = {
+  navbar: (theme: Theme) => ({
+    display: 'flex',
+    background: theme.colors.white,
+    height: '52px',
+    alignItems: 'center',
+    zIndex: 999,
+    padding: '0 12px',
+    justifyContent: 'space-between',
+    '@media (min-width:767px)': {
+      flexDirection: 'column' as const,
+      width: '72px',
+      height: '100vh',
+      position: 'fixed' as const,
+      left: '0%',
+      top: '0%',
+      right: 'auto',
+      bottom: '0%',
+      padding: 0,
+    },
+  }),
+  logo: {
+    width: '32px',
+    '@media (min-width: 767px)': {
+      display: 'none',
+    },
+  },
+};
