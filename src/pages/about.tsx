d827e9a437dbdf4633bf62ff28d6f7faf369bab5
Melinda Golden <info@melindagolden.com>
2020-02-23 22:48:20 -0500

    add: Resume link (#9)
    This PR:
    - Add resume link

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -123,7 +123,15 @@ const AboutPage: React.FC = () => {
                 <li>GraphQL</li>
               </ul>
             </div>
-            <LinkButton text="Download resume" action={() => {}} />
+            <LinkButton
+              text="Download resume"
+              action={() =>
+                window.open(
+                  'https://melindagolden.com/MelindaGolden.pdf',
+                  '_blank'
+                )
+              }
+            />
           </div>
         </div>
       </div>
